677fccf2167d504af70430617a817a05a157e10d
ngseke <ngseke@gmail.com>
2024-02-28 18:48:29 +0800

    chore: setup ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'only-multiline',
+    }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+}
